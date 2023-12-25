e6c09fb36d6065091e2475769a2d6ab540da691a
Zach <unixbird@racky.systems>
2023-12-25 13:48:08 -0600

    Update birdbot.py
    Found that if someone makes a discord and just copies your defined roles then they can execute these commands if they have your bot on the server.
    
    However unlikely this tightens the security a little.

--- birdbot.py
+++ birdbot.py
@@ -7,39 +7,29 @@ from discord.ext import commands
 prefix = os.environ.get('PREFIX')
 bot = commands.Bot(command_prefix=(prefix))
 
+#this simply just echos the current server where this command was executed
 @bot.command()
-@commands.has_role("AEG Swagmin")
+@commands.has_role("YOURDISCORDROLE")
 async def hello(ctx):
     await ctx.send(ctx.guild)
 
+#simple command to see where the host is and if its up
 @bot.command()
 async def test(ctx):
     hostname = os.environ.get('HOSTNAME')
     await ctx.send(hostname+ ' is alive')
 
+# example command on how to restrict to a specific role in discord in a specific server and executing a simple kubectl rollout restart command
 @bot.command()
-@commands.has_role("AEG Swagmin")
+@commands.has_role("YOURDISCORDROLE")
 async def zomboidrestart(ctx):
-    result = subprocess.call(["kubectl", 'rollout', 'restart', 'deployment/zomboid-deployment'])
-    if result == 0:
+    discordserverid = YOURDISCORDSERVERID
+    if server.id == discordserverid:
+      result = subprocess.call(["kubectl", 'rollout', 'restart', 'deployment/zomboid-deployment'])
       await ctx.send("executed restart of Zomboid")
     
-@bot.command()
-@commands.has_role("AEG Swagmin")
-async def arkrestart(ctx):
-    result = subprocess.call(["kubectl", 'rollout', 'restart', 'deployment/ark-deployment'])
-    if result == 0:
-      await ctx.send("executed restart of the Ark Server")
-                     
-@bot.command()
-@commands.has_role("SysOps")
-async def librespeedrestart(ctx):
-    result = subprocess.call(["kubectl", 'rollout', 'restart', 'deployment/librespeed-deployment'])
-    if result == 0:
-      await ctx.send("redeployed Librespeed")
-
 # ------------------------------
-# beyond this point is code before the change to bot.commands and is to only be used as reference! It is HIGHLY recommended to use bot.commands instead
+# beyond this point is code before the change to bot.commands and is to only be used as reference! It is HIGHLY recommended to use bot.command instead
 async def on_message(message):
 #   example command for the bot to execute a script on the host if used outside of a container or if the script exists on the container
     if message.content.startswith("/brio-start"):
